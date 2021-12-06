b7469c15df0e91a788aa5f5226c732db2063a49c
Zhongshan Shi <55010634+ZhongshanShi@users.noreply.github.com>
2021-12-05 22:41:26 -0500

    check for git connection

--- TradeShiftAPI/src/main/java/com/example/tradeshift/controller/tradeshiftController.java
+++ TradeShiftAPI/src/main/java/com/example/tradeshift/controller/tradeshiftController.java
@@ -23,7 +23,7 @@ public class tradeshiftController {
 
     @GetMapping("/healthCheck")
     public ResponseEntity<ResponseMessage> healthCheck(){
-        String message = "healthcheck successful";
+        String message = "healthcheck for main";
         return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(200,message));
     }
 
